85d83c0c9324e3cb4303e6fffb468515abad2ec9
sikandar <sikanderafridi.854@gmail.com>
2026-02-12 15:56:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todo.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
